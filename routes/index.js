6a5e3bb915ecbe3e12709bc5fa5ef9586f6dd194
Andy Herzog <asherzog19@gmail.com>
2017-01-03 14:28:01 -0700

    added cors and request modules

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request('https://api.parkwhiz.com/search/?lat=39.7576811&lng=-105.0068223&max_distance=10000&start=1483398004&end=1483398500&key=62d882d8cfe5680004fa849286b6ce20',
+            function(error, response, body) {
+              res.send(body);
+            });
 });
 
 module.exports = router;
