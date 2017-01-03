ad077251b6d6efd645bd0074ef40937e3255309c
“lukemccrae” <lukemccrae@gmail.com>
2017-01-03 11:07:44 -0700

    added get by id and post route

--- db/query.js
+++ db/query.js
@@ -1,7 +1,15 @@
 const knex = require('./knex');
 
 module.exports = {
-  getAll: function() {
-    return knex('spot');
-  }
+    getAll: function() {
+        return knex('spot');
+    },
+    getById: function(id) {
+        return knex('spot').where('user_id', id)
+    },
+    create: function(spot) {
+        return knex('spot')
+            .insert(spot, 'id')
+    },
+
 };
--- routes/spots.js
+++ routes/spots.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
 var spots = require('../db/query.js');
 
 router.get('/', (req, res) => {
-  spots.getAll()
-    .then(result => {
-      res.json(result);
-    });
+    spots.getAll()
+        .then(result => {
+            res.json(result);
+        });
 });
 
+router.get('/:id', (req, res, next) => {
+    spots.getById(req.params.id)
+        .then(result => {
+            res.json(result)
+        })
+})
 
+router.post('/', (req, res, next) => {
+    spots.create(spot)
+        .then(ids => {
+            const id = ids[0];
+            window.location = '/spots/' + id;
+        })
+})
 
 module.exports = router;
