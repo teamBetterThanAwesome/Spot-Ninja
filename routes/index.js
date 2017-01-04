f364cd2b20d6b400cbb3dea5906d21877bcf2599
“lukemccrae” <lukemccrae@gmail.com>
2017-01-04 13:13:08 -0700

    working on boundary box

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var GeoPoint = require('geopoint');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // userLocation = new Geopoint(${req.query.userLat}, ${req.query.userLng}).boundingCoordinates(2000) work on this later!
+  // console.log(userLocation);
+    console.log(req.query);
+    request(`https://api.parkwhiz.com/search/?lat=${req.query.userLat}&lng=${req.query.userLng}&max_distance=10000&start=1483398004&end=1483398500&key=62d882d8cfe5680004fa849286b6ce20`,
+        function(error, response, body) {
+            res.send(body);
+        });
 });
 
 module.exports = router;
