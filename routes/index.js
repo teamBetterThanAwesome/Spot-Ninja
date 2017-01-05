606f74312fc35cce455f581024ec13c5c0813552
“lukemccrae” <lukemccrae@gmail.com>
2017-01-04 21:09:35 -0700

    calling parkwhiz api in a larger area

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,31 @@ var router = express.Router();
 var request = require('request');
 var GeoPoint = require('geopoint');
 
+console.log("index js");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // userLocation = new Geopoint(${req.query.userLat}, ${req.query.userLng}).boundingCoordinates(2000) work on this later!
-  // console.log(userLocation);
-    console.log(req.query);
-    request(`https://api.parkwhiz.com/search/?lat=${req.query.userLat}&lng=${req.query.userLng}&max_distance=10000&start=1483398004&end=1483398500&key=62d882d8cfe5680004fa849286b6ce20`,
+    var userLocation = new GeoPoint(Number(req.query.userLat), Number(req.query.userLng)).boundingCoordinates(2)
+    var coordinates = {
+        topLeft: {
+            lat: userLocation[1]._degLat,
+            lng: userLocation[0]._degLon
+        },
+        bottomRight: {
+            lat: userLocation[0]._degLat,
+            lng: userLocation[1]._degLon
+        },
+        topRight: {
+            lat: userLocation[1]._degLat,
+            lng: userLocation[1]._degLon
+        },
+        bottomLeft: {
+            lat: userLocation[0]._degLat,
+            lng: userLocation[0]._degLon
+        }
+    };
+    // console.log(coordinates.topLeft, coordinates.topRight, coordinates.bottomLeft, coordinates.bottomRight);
+    request(`https://api.parkwhiz.com/v3_1/quotes/?start_time=2017-01-04T10:30:00&end_time=2017-01-04T13:30:00&email=&q=anchor_coordinates:${req.query.userLat},${req.query.userLng}%20search_type:transient%20bounds:${coordinates.topLeft.lat},${coordinates.topLeft.lng},${coordinates.topRight.lat},${coordinates.topRight.lng},${coordinates.bottomLeft.lat},${coordinates.bottomLeft.lng},${coordinates.bottomRight.lat},${coordinates.bottomRight.lng}%20venue_id:185768`,
         function(error, response, body) {
             res.send(body);
         });
